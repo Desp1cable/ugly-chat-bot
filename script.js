db5d0486300042ca2e03213ca2e447204f25b027
Desp1cable <Sagaidullaev_pr1.19@st.ithub.ru>
2021-10-12 12:10:00 +0300

    switch added

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const messages = document.querySelector(".messages")
 
 mess.addEventListener("keydown", function (e) {
 
-  console.log(e, )
-
   if(e.keyCode == 13) {
+
+    let res
+
+    switch (this.value.toLowerCase()) {
+      case "привет": res = "Привет!"; break;
+      case "как дела?": res = "Хорошо! У тебя как?"; break;
+      case "как дела": res = "Хорошо! У тебя как?"; break;
+      case "тоже хорошо": res = "Это отлично!"; break;
+      case "хорошо": res = "Это отлично!"; break;
+      case "пока": res = "Пока!"; break;
+      case "уву": res = "UWU-UWU!"; break;
+      default: res = "Okey, I got you..."; break;
+    }
+
     messages.innerHTML += `
       <div class="message message-you">
       <img class="img" src="./images/you.png">
@@ -21,14 +33,13 @@ mess.addEventListener("keydown", function (e) {
       <img class="img" src="./images/bot.png">
       <div class="message-text">
         <p class="text">
-          Okey, I got you...
+          ${res}
         </p>
         <p class="time">15:01</p>
       </div>
     </div>
     `
-
-
+    this.value = ""
   }
 
 })
